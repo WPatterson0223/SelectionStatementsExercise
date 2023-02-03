876ed8fe4ac763aa12bd77befcac31d78dccfc02
Will Patterson <wlp223@aol.com>
2023-02-03 12:36:07 -0500

    exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var num = new Random();
+            var randomNum = num.Next(1, 4);
+
+            Console.WriteLine("Guess a number 1-3");
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess < randomNum)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (guess > randomNum)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
